59b7fe3567f1870740fe238035d33fd34b54dbf4
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-05-05 19:27:37 +0200

    Adapted text to new function

--- views/shared/index/index.php
+++ views/shared/index/index.php
@@ -1,6 +1,6 @@
 <?php echo head(array('title' => __('Collection Tree'))); ?>
 <?php if ($full_collection_tree = $this->full_collection_tree): ?>
-<p><?php echo __('Here\'s a view of the Collections\' tree. Click on any Collection\'s name to open it, and on any folder to expand/contract the tree.'); ?></p>
+<p><?php echo __('Here\'s a view of the Collections\' tree. Click on any Collection\'s name to open it, and on any folder with a plus sign to expand/collapse that branch.'); ?></p>
 <?php echo $full_collection_tree; ?>
 <?php else: ?>
 <p><?php echo __('There are no collections.'); ?></p>
