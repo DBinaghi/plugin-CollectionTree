166fc33ccd3d1fe16c9aaee100a6976af900ae3f
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-05-05 19:24:20 +0200

    Added tooltips for expand/collapse

--- views/shared/javascripts/file-explore.js
+++ views/shared/javascripts/file-explore.js
@@ -1,20 +1,25 @@
 jQuery(document).ready(function () {
-	init();
-
-	function init() {
-		var startStatus = (jQuery("#collection-tree").hasClass("treeExpanded") ? "expanded" : "collapsed");
-
-		if (startStatus == "collapsed") {
-			jQuery("#collection-tree ul:not(:first)").hide();
-		}		
-
-		jQuery("#collection-tree li").prepend("<span class='collection-tree-icon handle'></span>");
-
-		jQuery("#collection-tree li:has(ul)")
-			.children(":first-child").addClass(startStatus)
-			.click(function(){    
-				jQuery(this).toggleClass("collapsed expanded")
-					.siblings("ul").toggle();
-			});       
+	if (jQuery("#collection-tree").hasClass("treeExpanded")) {
+		var treeClass = "expanded";
+		var treeTitle = "collapse";
+	} else {
+		var treeClass = "collapsed";
+		var treeTitle = "expand";
+		jQuery("#collection-tree ul:not(:first)").hide();
 	}
+
+	jQuery("#collection-tree li").prepend("<span class='collection-tree-icon handle'></span>");
+
+	jQuery("#collection-tree li:has(ul)")
+		.children(":first-child").addClass(treeClass)
+		.attr('title', treeTitle)
+		.click(function(){    
+			jQuery(this).toggleClass("collapsed expanded")
+				.siblings("ul").toggle();
+			if (treeTitle == "collapse") {
+				jQuery(this).attr('title', "expand");
+			} else {
+				jQuery(this).attr('title', "collapse");
+			}
+		});       
 });
